7d8d1c40df00d484f2968fb2eed47a11ace62567
Rene van der Ark <r.van.der.ark@gmail.com>
2012-05-22 16:39:16 +0200

    added wikisearch

---
+++ client/js/wikisearch.js
@@ -0,0 +1,45 @@
+(function( $ ) {
+$.fn.wikiSearch = function(opts) {
+	var labelTxt = Util.readOpt("label", "Search Wikipedia");
+	var lang = Util.readOpt("lang", "en");
+	var label = $("<label>").html(labelTxt);
+	var input = $("<input>");
+	
+	var resultList = $("<ul>");
+	var listResults = function(results) {
+		resultList.html("");
+		if(results.length > 1 && results[1].length > 0) {
+			$.each(results[1], function(i, hit) {
+				var item = $("<li>").append(hit);
+				resultList.append(item);
+			});
+		} else {
+			resultList.html("<li>no hits</li>");
+		}
+	};
+
+	var flagLink = $("<a>").append($("<img>").attr("src", "img/" + lang + ".png")).css({"float": "right", cursor: "pointer"});
+	var flagPicker = $("<div>").css({"float": "right", display: "none"});
+	flagLink.click(function(e) { flagPicker.show(); $(this).hide();});
+	$.each(["nl", "en", "fr", "de"], function(i, language) {
+		var flagSetter = $("<a>").append($("<img>").attr("src", "img/" + language + ".png")).css({cursor: "pointer"});
+		flagSetter.click(function(e) {
+			lang = language;
+			flagLink.html($("<img>").attr("src", "img/" + language + ".png"));
+			input.val("");
+			flagPicker.hide();
+			flagLink.show();
+		});
+		flagPicker.append(flagSetter);
+	});
+	this.append(flagPicker).append(flagLink).append(label).append("<br>").append(input).append(resultList);
+	input.on("change", function(e) {
+		if(this.value.length > 0) {
+			$.ajax("https://" + lang + ".wikipedia.org/w/api.php", {
+				data: {action: "opensearch", search: this.value, limit: 10, namespace:0, format: "json"},
+				dataType: "jsonp",
+				success: listResults
+			});
+		}
+	});
+}})(jQuery);
