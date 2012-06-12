5834c01cff2b3ec58f200f4837f92cfbd5f7de4a
Rene van der Ark <r.van.der.ark@gmail.com>
2012-06-12 17:19:50 +0200

    pagination

--- client/js/portalsearch.js
+++ client/js/portalsearch.js
@@ -3,12 +3,24 @@ var PortalSearch = function(key, opts) {
 	var container = opts.resultBox;
 	var spinner = opts.spinner;
 	var lastSearchQ = null;
-	var showResults = function(data) {
+
+	var paginate = function(q, page, resultsContainer) {
+			$.ajax("http://api.europeana.eu/api/opensearch.json", {
+				data: {wskey: apiKey, searchTerms: q, startPage: page},
+				dataType: "jsonp",
+				success: function(data, success, state) {
+					showResults(data, success, state, resultsContainer);
+				}
+			});
+	};
+
+	var showResults = function(data, success, state, resultsContainer) {
 		$("#" + spinner).hide();
 		if(data && data.totalResults > 0) {
-			$(".search-result .resultset").hide();
-			var resultsTag = $("<div>").addClass("resultset");
-			var descrTag = $("<a>").html(data.description.replace("- Europeana Open Search","") + "(" + data.totalResults + ")").click(function(e) {
+			var returnQ = data.description.replace(" - Europeana Open Search","");
+			if(!resultsContainer) { $(".search-result .resultset").hide(); }
+			var resultsTag = resultsContainer ? resultsContainer.html("") : $("<div>").addClass("resultset");
+			var descrTag = $("<a>").html(returnQ + " (" + data.totalResults + ")").click(function(e) {
 				resultsTag.toggle();
 			});
 			var div = $("<div>").addClass("search-result");
@@ -18,8 +30,23 @@ var PortalSearch = function(key, opts) {
 				resultsTag.append($("<li>").append(link).append(meta));
 			});
 
-			div.append(descrTag).append(resultsTag);
-			$("#" + container).prepend(div);
+			if(data.startIndex + data.itemsPerPage < data.totalResults) {
+				var pagLink = $("<a>").html("&gt;&gt;").css({"float": "right"}).click(function(e) {
+					paginate(returnQ, (data.startIndex / data.itemsPerPage) + 2, resultsTag);
+				});
+				resultsTag.append(pagLink);
+			}
+			if(data.startIndex > 0) {
+				var pagLink =  $("<a>").html("&lt;&lt;").css({"float": "left"}).click(function(e) {
+					paginate(returnQ, (data.startIndex / data.itemsPerPage), resultsTag);
+				});
+				resultsTag.append(pagLink);
+			}
+			resultsTag.append($("<div>").css({clear: "both"}));
+			if(!resultsContainer) {
+				div.append(descrTag).append(resultsTag);
+				$("#" + container).prepend(div);
+			}
 		}
 	};
 
